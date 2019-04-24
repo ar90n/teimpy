3ff7035bec9828f13020b273e8a19aef6522c686
Masahiro Wada <argon.argon.argon@gmail.com>
2019-04-24 20:22:02 +0000

    Remove Python3.7 support

--- setup.py
+++ setup.py
@@ -51,6 +51,5 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
 )
