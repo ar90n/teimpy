1304d971eb9d1988dfa47545df98ae264ae67aba
Masahiro Wada <argon.argon.argon@gmail.com>
2020-02-11 16:34:59 +0000

    Use libsixel

---
+++ build.py
@@ -0,0 +1,32 @@
+import os
+import urllib.request
+
+ARCHIVE_FILE = "libsixel-1.8.6.tar.gz"
+LIBSIXEL_DIR = "sixel-1.8.6"
+
+
+def build(setup_kwargs):
+    url = f"https://github.com/saitoha/libsixel/releases/download/v1.8.6/{ARCHIVE_FILE}"
+    urllib.request.urlretrieve(url, f"/tmp/{ARCHIVE_FILE}")
+
+    commands = f"""tar -zxvf {ARCHIVE_FILE} && \
+cd {LIBSIXEL_DIR} && \
+./configure && \
+make -j && \
+cp ./src/.libs/libsixel.so ../src/teimpy/libsixel/libsixel.so
+"""
+
+    os.system(commands)
+    setup_kwargs.update(
+        {
+            "ext_modules": EmptyListWithLength(),
+            "package_data": {"": ["libsixel/libsixel.so"]},
+            "include_package_data": True,
+        }
+    )
+
+
+# from opencv-python
+class EmptyListWithLength(list):
+    def __len__(self):
+        return 1
--- src/teimpy/impl/sixel.py
+++ src/teimpy/impl/sixel.py
@@ -1,19 +1,28 @@
 from io import StringIO, BytesIO
-from sixel import SixelWriter
 from PIL import Image
+import numpy as np
 
+from .. import libsixel
 from .base import DrawerBase
 
 
 class SixelDrawer(DrawerBase):
     def draw(self, buffer, shape=None, preserve_aspect_ratio=True):
-        input = BytesIO()
-        img = Image.fromarray(buffer)
-        img.save(input, "PNG")
-        input.seek(0)
+        if buffer.dtype not in [np.uint8]:
+            raise ValueError("BlockDrawer only supports np.uin8.")
 
-        output = StringIO()
-        output.fileno = lambda: 1  # Hack for PySixel
+        height, width = buffer.shape[0], buffer.shape[1]
+        bytes = buffer.tobytes()
+        if len(buffer.shape) == 3:
+            dither = libsixel.sixel_dither_new(256)
+            libsixel.sixel_dither_initialize(
+                dither, bytes, width, height, libsixel.SIXEL_PIXELFORMAT_RGB888
+            )
+        else:
+            dither = libsixel.sixel_dither_get(libsixel.SIXEL_BUILTIN_G8)
+            libsixel.sixel_dither_set_pixelformat(dither, libsixel.SIXEL_PIXELFORMAT_G8)
 
-        SixelWriter().draw(input, output=output)
-        return output.getvalue()
+        s = BytesIO()
+        output = libsixel.sixel_output_new(lambda data, s: s.write(data), s)
+        libsixel.sixel_encode(bytes, width, height, 1, dither, output)
+        return s.getvalue().decode("ascii")
